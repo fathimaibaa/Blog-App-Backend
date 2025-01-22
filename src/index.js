cfffa1835601e48f77bb80128669826e7e3fe27e
hibaa <fathimaibaa@gmail.com>
2025-01-22 12:51:36 +0530

    script changed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const {PORT} = secrets
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://blog-app-frontend-lime.vercel.app/',
     credentials:true
 }))
 app.use(cookieParser())
